50792acb512bc99c627879fda3850f521673b021
Aymane <aymanecheikh2002@gmail.com>
2025-11-14 18:40:51 +0000

    rejigging the tempdata dataclass and added a gendata parent class

--- src/sensor_data/main.py
+++ src/sensor_data/main.py
@@ -1,24 +1,18 @@
 from dataclasses import dataclass, field
 import numpy as np
-
 generator = np.random.default_rng(1)
 
+
+class GenData:
+    num_readings: int = generator.integers(1_000_000, 10_000_001)
+
+
 @dataclass
-class TemperatureData:
-    num_readings: int = field(init=False)
-    mean_temp: float = field(init=False)
-    std_dev_temp: float = field(init=False)
-    data: np.ndarray = field(init=False)
-    mean: float = field(init=False)
-    median: float = field(init=False)
-    std: float = field(init=False)
-    fifth_percentile: float = field(init=False)
-    ninetyfifth_percentile: float = field(init=False)
+class TemperatureData(GenData):
+    mean_temp: float = field(default=generator.uniform(40, 50), init=False)
+    std_dev_temp: float = field(default=generator.uniform(10, 15), init=False)
 
     def __post_init__(self):
-        self.num_readings = generator.integers(1_000_000, 10_000_001)
-        self.mean_temp = generator.uniform(40, 50)
-        self.std_dev_temp = generator.uniform(10, 15)
         self.data = generator.normal(
             loc=self.mean_temp,
             scale=self.std_dev_temp,
@@ -30,18 +24,17 @@ class TemperatureData:
         self.fifth_percentile = np.percentile(self.data, 5)
         self.ninetyfifth_percentile = np.percentile(self.data, 95)
 
-
     def __repr__(self):
         return (
             '--- Temperature Statistics ---\n'
             f'Mean (Average): {self.mean:.2f} °C\n'
             f'Median (Middle): {self.median:.2f} °C\n'
             f'Std. Deviation (Spread): {self.std:.2f} °C\n'
-            f'90% Normal Range: {self.fifth_percentile:.2f} °C'
-            f'to {self.ninetyfifth_percentile:.2f} °C'
+            f'90% Normal Range: {self.fifth_percentile:.2f} °C '
+            f'to {self.ninetyfifth_percentile:.2f} °C\n'
         )
 
 
 if __name__ == '__main__':
     x = TemperatureData()
-    print(x)
+    pressure_data = generator.uniform(low=100, high=500, size=x.num_readings)
