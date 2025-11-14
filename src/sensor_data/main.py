2d593fdd298984b827728926f149e6f07977ebbb
Aymane <aymanecheikh2002@gmail.com>
2025-11-14 19:00:09 +0000

    added pressure data and did some further decoupling

--- src/sensor_data/main.py
+++ src/sensor_data/main.py
@@ -3,9 +3,30 @@ import numpy as np
 generator = np.random.default_rng(1)
 
 
+@dataclass
+class DataDescriptor:
+    data: np.ndarray
+
+    def __post_init__(self):
+        self.mean = np.mean(self.data)
+        self.median = np.median(self.data)
+        self.std = np.std(self.data)
+        self.fifth_percentile = np.percentile(self.data, 5)
+        self.ninetyfifth_percentile = np.percentile(self.data, 95)
+
+    def __repr__(self, data_name):
+        return (
+            f'--- {data_name} Statistics ---\n'
+            f'Mean (Average): {self.mean:.2f} °C\n'
+            f'Median (Middle): {self.median:.2f} °C\n'
+            f'Std. Deviation (Spread): {self.std:.2f} °C\n'
+            f'90% Normal Range: {self.fifth_percentile:.2f} °C '
+            f'to {self.ninetyfifth_percentile:.2f} °C\n'
+        )
+
+
 class GenData:
     num_readings: int = generator.integers(1_000_000, 10_000_001)
-
 
 @dataclass
 class TemperatureData(GenData):
@@ -18,23 +39,25 @@ class TemperatureData(GenData):
             scale=self.std_dev_temp,
             size=self.num_readings
         )
-        self.mean = np.mean(self.data)
-        self.median = np.median(self.data)
-        self.std = np.std(self.data)
-        self.fifth_percentile = np.percentile(self.data, 5)
-        self.ninetyfifth_percentile = np.percentile(self.data, 95)
+        self.stats = DataDescriptor(self.data)
 
     def __repr__(self):
-        return (
-            '--- Temperature Statistics ---\n'
-            f'Mean (Average): {self.mean:.2f} °C\n'
-            f'Median (Middle): {self.median:.2f} °C\n'
-            f'Std. Deviation (Spread): {self.std:.2f} °C\n'
-            f'90% Normal Range: {self.fifth_percentile:.2f} °C '
-            f'to {self.ninetyfifth_percentile:.2f} °C\n'
+        return self.stats.__repr__('Temperature')
+
+@dataclass
+class PressureData(GenData):
+    def __post_init__(self):
+        self.data = generator.uniform(
+            low=100,
+            high=500,
+            size=self.num_readings
         )
+        self.stats = DataDescriptor(self.data)
+
+    def __repr__(self):
+        return self.stats.__repr__('Pressure')
 
 
 if __name__ == '__main__':
-    x = TemperatureData()
-    pressure_data = generator.uniform(low=100, high=500, size=x.num_readings)
+    t = TemperatureData()
+    p = PressureData()
